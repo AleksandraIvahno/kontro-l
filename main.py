1837c4a1e2ea394fffa93861f28363673c62eaab
AleksandraIvahn <91884342+AleksandraIvahno@users.noreply.github.com>
2022-02-28 12:47:24 +0000

    #10

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-i = int (input("Ievadi cik ir "))
-sum_ = 0
-len_ = 0
-while i>0:
-  al = int(input("Ievadi vienu atzīmi "))
-  i = i-1
-  sum_+=i-1
-  len_+=1
-print(sum_/len_)
+a = float(input("Ievadiet jūsu svaru(N): "))
+b = float(input("Ievadiet jūsu svaru(N): "))
+c = float(input("Ievadiet jūsu svaru(N): "))
+x = a* 0.10197
+y = b* 0.10197
+z = c* 0.10197
+print("Jūsu masa ir", round(x,2))
+print("Jūsu masa ir", round(y,2))
+print("Jūsu masa ir", round(z,2))
+d = [x,y,z]
+print("Jūsu kopēja masa ir", round(sum(d),2))
+print("Jūsu kopēja masa ir", round(sum(d)/len(d),2))
+if sum(d)<300:
+  print("Jūs varat braukt ar liftu")
+else:
+  print("Jūs nevarat braukt ar liftu")
