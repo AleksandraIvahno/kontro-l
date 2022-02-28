c9aba32027099b442ec00a196b38556fa6bd0266
AleksandraIvahn <91884342+AleksandraIvahno@users.noreply.github.com>
2022-02-28 12:28:21 +0000

    #8

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-a = input("Ievadi vardu: ")
-b = 1
-while b< 23:
-  print (b,a)
-  b=b+1
-while b< 24:
-   print (a)
-   b=b+(1/22)
-while b < 25:
-      print (a, end= ' ')
-      b=b+(1/22)
-
+i = 1
+while i <=10:
+  print(i**2,)
+  i = i + 1
+i = 1
+while i <=15 :
+  print (i**3,)
+  i=i+1
+i = 2 
+while i <=20:
+  print (i**0.5)
+  i=i+1
